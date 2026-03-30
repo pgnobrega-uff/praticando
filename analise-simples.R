ceeb770c71434e91f31eb8e44325e5a685682cc2
Pedro Nobrega <pgnobrega@id.uff.br>
2026-03-30 09:22:21 -0300

    Script inicial com leitura do banco de dados

---
+++ analise-simples.R
@@ -0,0 +1,6 @@
+# Script referente a aula prática de 30/03 de Git e GitHub, vinculado ao projeto
+Praticando
+
+# Leitura do banco de dados
+dados = read.csv("dados - respostas formulário.csv", header=T, sep=";")
+attach(dados)
